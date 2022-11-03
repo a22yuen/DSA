1b020d484e07463224535e11b5665b4307cab0da
Anson Yuen <a22yuen@uwaterloo.ca>
2022-11-02 23:56:52 -0700

    11 ctonains most water solution

---
+++ two-pointers/11-contains-most-water.py
@@ -0,0 +1,32 @@
+# -----------------------------------------------------------
+# 11. Container With Most Water
+# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+# Return the maximum amount of water a container can store.
+# Notice that you may not slant the container.
+
+# Example 1:
+#   Input: height = [1,8,6,2,5,4,8,3,7]
+#   Output: 49
+#   Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+# Example 2:
+#   Input: height = [3,2,8,2,2,2,8,2,1,1]
+#   Output: 49
+# -----------------------------------------------------------
+from typing import List 
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxarea = 0
+        left = 0
+        right = len(height) - 1
+        
+        while left < right:
+            width = right - left
+            maxarea = max(maxarea, min(height[left], height[right]) * width)
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+                
+        return maxarea
