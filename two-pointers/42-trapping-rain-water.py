4cdd2a0981336a7f29b89ee3fdda6957cd04a183
Anson Yuen <a22yuen@uwaterloo.ca>
2022-11-04 14:51:19 -0700

    initial trapping rain water solution

--- two-pointers/42-trapping-rain-water.py
+++ two-pointers/42-trapping-rain-water.py
@@ -9,3 +9,43 @@
 #   Input: height = [4,2,0,3,2,5]
 #   Output: 9
 # -----------------------------------------------------------
+
+# Initial Solution
+# Doesn't account for buckets inside buckets
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l = 0
+        r = 0
+        total = 0
+        while r < len(height) and l < len(height)-1:
+            # search for first side of bucket
+            print("start left", l)
+            if height[l] <= height[l+1]:
+                if l >= len(height)-2:
+                    return total
+                l += 1
+                continue
+            # search for second side of bucket
+            r = l+2
+            while r < len(height) and height[r] < height[l]:
+                print("find right", r)
+                r += 1
+            if r >= len(height):
+                print("cant find")
+                l += 1
+                print("cant", l)
+                r = l
+                continue
+            newl = l
+            newr = r
+            while newl < newr:
+                print("newl", newl)
+                print("newr", newr)
+                diff = newr-newl-1
+                total += diff
+                newl = newl+1
+                newr = newr-1
+                
+            l = r
+        return total
+            
