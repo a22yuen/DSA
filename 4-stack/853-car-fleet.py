ab864d2b1ef8426ff3c949c986beb6768bc8deb8
Anson Yuen <a22yuen@uwaterloo.ca>
2022-12-06 09:38:14 -0500

    better car fleet solution

--- 4-stack/853-car-fleet.py
+++ 4-stack/853-car-fleet.py
@@ -13,3 +13,14 @@ class Solution:
             if len(r) == 0 or not catch(r[-1], x):
                 r.append(x)
         return len(r)
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        ps = sorted(list(zip(position, speed)), reverse=True)
+        time, r = 0, 0
+        for pos, spe in ps:
+            t = (target-pos)/spe
+            if t > time:
+                time = t
+                r += 1
+        return r
